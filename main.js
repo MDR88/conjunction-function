7e19890708753727d6ac7022ea0384138e0fd561
mdroberts74 <mikedroberts88@gmail.com>
2018-06-22 15:29:50 -0500

    My first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// Conjunction Function
+
+
+const conjunction = function(firstWord, secondWord) {
+    const conjoinedWord = `${firstWord} ${secondWord}`
+}
+
+conjunction("Master", "Card")
+console.log(conjoinedWord)
