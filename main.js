b768fe8387cc1a9272dfd338b8de17ebbd7d97de
mdroberts74 <mikedroberts88@gmail.com>
2018-06-24 18:47:48 -0500

    I moved the console log and ran the funcion inside the curly braces. I also changed line 6 from const to let.

--- main.js
+++ main.js
@@ -3,13 +3,16 @@
 
 
 const conjunction = function(firstWord, secondWord) {
-        const conjoinedWord = `${firstWord} ${secondWord}`
+    let conjoinedWord = `${firstWord} ${secondWord}`
 
 
-        conjunction("Master", "Card")
-        console.log(conjoinedWord)
 
-    }
-    //I moved these inside the curly braces to stay in ScopedCredential.
-    // conjunction("Master", "Card")
-    // console.log(conjoinedWord)
+    conjunction("Master", "Card")
+    console.log(conjoinedWord)
+}
+
+
+
+//I moved these inside the curly braces to stay in ScopedCredential. I also changed line 6 from const to let.
+// conjunction("Master", "Card")
+// console.log(conjoinedWord)
